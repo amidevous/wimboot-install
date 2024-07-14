d9fc91898c5bc837f4cd326797bdd83b59c869eb
amidevous <amidevous@users.noreply.github.com>
2024-07-14 03:37:29 +0200

    Update install.bat

--- win7/autounattend/x86/starter/french/legacy/install.bat
+++ win7/autounattend/x86/starter/french/legacy/install.bat
@@ -94,9 +94,9 @@ rem mkdir s:\sources\microsoft-windows-tapisetup\
 rem mkdir s:\sources\microsoft-windows-terminalservices-licenseserver\
 rem mkdir s:\sources\microsoft-windows-textservicesframework-migration-dl\
 rem mkdir s:\sources\microsoft-windows-textservicesframework-migration-dl\
-setup-x86.exe --no-admin --root X:\Cygwin\ --quiet-mode --no-shortcuts --no-startmenu --allow-unsupported-windows --arch x86 --force-current --no-desktop --no-replaceonreboot --no-verify --no-version-check --no-warn-deprecated-windows --no-write-registry --only-site --site https://mirrors.kernel.org/sourceware/cygwin-archive/20221123/ -l X:\Cygwin\var\cache\apt\packages --packages dos2unix,wget,ca-certificates
+setup-x86.exe --no-admin --root S:\Cygwin\ --quiet-mode --no-shortcuts --no-startmenu --allow-unsupported-windows --arch x86 --force-current --no-desktop --no-replaceonreboot --no-verify --no-version-check --no-warn-deprecated-windows --no-write-registry --only-site --site https://mirrors.kernel.org/sourceware/cygwin-archive/20221123/ -l S:\Cygwin\var\cache\apt\packages --packages dos2unix,wget,ca-certificates
 rem cd s:\
-X:\Cygwin\bin\wget.exe "https://archive.org/download/windows_7_professional_with_sp1_original_latest_iso_multilanguage/fr_windows_7_professional_with_sp1_x86_dvd_u_677092.iso" -O "s:\fr_windows_7_professional_with_sp1_x86_dvd_u_677092.iso"
+S:\Cygwin\bin\wget.exe "https://archive.org/download/windows_7_professional_with_sp1_original_latest_iso_multilanguage/fr_windows_7_professional_with_sp1_x86_dvd_u_677092.iso" -O "s:\fr_windows_7_professional_with_sp1_x86_dvd_u_677092.iso"
 7z x fr_windows_7_professional_with_sp1_x86_dvd_u_677092.iso
 rem rclone copy http:\sources s:\sources -vv
 rem start rclone mount http: y: --file-perms=0777 --dir-perms 0777 --network-mode --buffer-size=0 --vfs-cache-max-size=500M -vv

21ab3ea96a2c9de26da5992fd6512a7ba7dde754
amidevous <amidevous@users.noreply.github.com>
2024-07-16 18:06:08 +0200

    Update manual.bat

--- win7/autounattend/x86/manual.bat
+++ win7/autounattend/x86/manual.bat
@@ -75,8 +75,7 @@ diskpart /s config.txt
 ping -n 1 google.fr
 setup-%arch1%.exe --no-admin --root S:\Cygwin\ --quiet-mode --no-shortcuts --no-startmenu --allow-unsupported-windows --arch %arch1% --force-current --no-desktop --no-replaceonreboot --no-verify --no-version-check --no-warn-deprecated-windows --no-write-registry --only-site --site %cygmirror% -l S:\Cygwin\var\cache\apt\packages --packages dos2unix,wget,ca-certificates
 S:\Cygwin\bin\wget.exe %giturl%/releases/download/%winversion%/%isoversionmin%min%arch1%.iso -O s:\%isoversionmin%min%arch1%.iso
-7z x -y s:\%gitbcommit%min%arch1%.iso -os:\
-cmd
+7z x -y s:\%isoversionmin%min%arch1%.iso -os:\
 S:\Cygwin\bin\wget.exe %giturl%/releases/download/%winversion%/install.swm -O s:\sources\install.swm
 S:\Cygwin\bin\wget.exe %giturl%/releases/download/%winversion%/install2.swm -O s:\sources\install2.swm
 S:\Cygwin\bin\wget.exe %giturl%/releases/download/%winversion%/install3.swm -O s:\sources\install3.swm

c935e2ec852f6fde543bfbf4ec8066b453613f2d
amidevous <amidevous@users.noreply.github.com>
2024-07-17 02:53:27 +0200

    Update install.bat

--- win7/autounattend/x86/starter/french/legacy/install.bat
+++ win7/autounattend/x86/starter/french/legacy/install.bat
@@ -4,6 +4,7 @@ set giturl=https://github.com/amidevous/wimboot-install
 set gitbcommit=main
 set winversion=windows7
 set winversionmin=w7
+set isoversionmin=win7
 set arch1=x86
 set arch2=x86
 set cygmirror=https://mirrors.kernel.org/sourceware/cygwin-archive/20221123/
@@ -28,9 +29,32 @@ if exist x:\Windows\System32\viogpudo.inf pnputil /i /a x:\Windows\System32\viog
 if exist x:\Windows\System32\vioinput.inf pnputil /i /a x:\Windows\System32\vioinput.inf
 if exist x:\Windows\System32\viorng.inf pnputil /i /a x:\Windows\System32\viorng.inf
 if exist x:\Windows\System32\vioser.inf pnputil /i /a x:\Windows\System32\vioser.inf
+setlocal enableDelayedExpansion
+for /f "delims=" %%a in ('ipconfig /all') do (
+    set line=%%a
+    if not "!line:~0,1!"==" " if not "!line:adapter=!"=="!line!" (
+        set name=!line:*adapter =!
+        set name=!name::=!
+    )
+    for /f "tokens=1,2,*" %%b in ("%%a") do (
+        if "%%b %%c"=="Physical Address." (
+            set mac=%%d
+            set mac=!mac:*: =!
+            echo !name!: !mac!
+            if "!mac!"=="52-54-00-01-13-9E" (
+                netsh interface ipv4 set address name="Ethernet" static 163.172.118.206 mask=255.255.255.255 gateway=62.210.202.1 10
+            )
+            else (
+                ipconfig /renew
+            )
+        )
+    )
+)
 net start dnscache
+netsh interface ipv4 set dns name="Ethernet" static 8.8.8.8 primary
+netsh interface ipv4 set winsservers name="Ethernet" static 8.8.8.8
+ipconfig /flushdns﻿
 wpeutil WaitForNetwork
-ipconfig /renew
 echo select disk 0 > config.txt
 echo clean >> config.txt
 echo create partition primary size=20000 >> config.txt
@@ -48,10 +72,10 @@ echo set id=27 >> config.txt
 echo list volume >> config.txt
 echo exit >> config.txt
 diskpart /s config.txt
+ping -n 1 google.fr
 setup-%arch1%.exe --no-admin --root S:\Cygwin\ --quiet-mode --no-shortcuts --no-startmenu --allow-unsupported-windows --arch %arch1% --force-current --no-desktop --no-replaceonreboot --no-verify --no-version-check --no-warn-deprecated-windows --no-write-registry --only-site --site %cygmirror% -l S:\Cygwin\var\cache\apt\packages --packages dos2unix,wget,ca-certificates
-rem cd s:\
-S:\Cygwin\bin\wget.exe %giturl%/releases/download/%winversion%/%gitbcommit%min.iso -O s:\%gitbcommit%min.iso
-7z x -y s:\%gitbcommit%min.iso -os:\
+S:\Cygwin\bin\wget.exe %giturl%/releases/download/%winversion%/%isoversionmin%min%arch1%.iso -O s:\%isoversionmin%min%arch1%.iso
+7z x -y s:\%isoversionmin%min%arch1%.iso -os:\
 S:\Cygwin\bin\wget.exe %giturl%/releases/download/%winversion%/install.swm -O s:\sources\install.swm
 S:\Cygwin\bin\wget.exe %giturl%/releases/download/%winversion%/install2.swm -O s:\sources\install2.swm
 S:\Cygwin\bin\wget.exe %giturl%/releases/download/%winversion%/install3.swm -O s:\sources\install3.swm
@@ -62,7 +86,7 @@ S:\Cygwin\bin\wget.exe %giturl%/releases/download/%winversion%/install7.swm -O s
 S:\Cygwin\bin\wget.exe %giturl%/releases/download/%winversion%/install8.swm -O s:\sources\install8.swm
 S:\Cygwin\bin\wget.exe %giturl%/releases/download/%winversion%/install9.swm -O s:\sources\install9.swm
 S:\Cygwin\bin\wget.exe %giturl%/releases/download/%winversion%/install10.swm -O s:\sources\install10.swm
-s:\sources\setup.exe /noreboot /unattend:x:\Windows\System32\autounattend.xml
+s:\sources\setup.exe /noreboot
 if %gitbcommit%==win10 Dism /Image:w:\ /enable-feature /featurename:NetFx3 /All /Source:"s:\sources\sxs" /LimitAccess /NoRestart /LogLevel:4
 if exist x:\Windows\System32\e1d68x64.inf Dism /Image:W:\ /Add-Driver /Driver:x:\Windows\System32\e1d68x64.inf
 if exist x:\Windows\System32\balloon.inf wget %giturl%/raw/%gitbcommit%/win10/drv/virtio-win-0.1.240/Balloon/%winversionmin%/%arch2%/balloon.cat -O x:\Windows\System32\balloon.cat
@@ -83,33 +107,15 @@ if exist x:\Windows\System32\VBoxMouse.inf Dism /Image:W:\ /Add-Driver /Driver:x
 if exist x:\Windows\System32\VBoxVideo.inf Dism /Image:W:\ /Add-Driver /Driver:x:\Windows\System32\VBoxVideo.inf
 if exist x:\Windows\System32\VBoxVideoEarlyNT.inf Dism /Image:W:\ /Add-Driver /Driver:x:\Windows\System32\VBoxVideoEarlyNT.inf
 if exist x:\Windows\System32\VBoxWddm.inf.inf Dism /Image:W:\ /Add-Driver /Driver:x:\Windows\System32\VBoxWddm.inf
-if %gitbcommit%==win10 if exist x:\Windows\System32\fwcfg.inf Dism /Image:W:\ /Add-Driver /Driver:x:\Windows\System32\fwcfg.inf
-if %gitbcommit%==win10 if exist x:\Windows\System32\pvpanic-pci.inf Dism /Image:W:\ /Add-Driver /Driver:x:\Windows\System32\pvpanic-pci.inf
-if %gitbcommit%==win10 if exist x:\Windows\System32\pvpanic.inf wget %giturl%/raw/%gitbcommit%/win10/drv/virtio-win-0.1.240/pvpanic/%winversionmin%/%arch2%/pvpanic.cat -O x:\Windows\System32\pvpanic.cat
-if %gitbcommit%==win10 if exist x:\Windows\System32\pvpanic.inf wget %giturl%/raw/%gitbcommit%/win10/drv/virtio-win-0.1.240/pvpanic/%winversionmin%/%arch2%/pvpanic.inf -O x:\Windows\System32\pvpanic.inf
-if %gitbcommit%==win10 if exist x:\Windows\System32\pvpanic.inf wget %giturl%/raw/%gitbcommit%/win10/drv/virtio-win-0.1.240/pvpanic/%winversionmin%/%arch2%/pvpanic.cat -O x:\Windows\System32\pvpanic.cat
-if %gitbcommit%==win10 if exist x:\Windows\System32\pvpanic.inf Dism /Image:W:\ /Add-Driver /Driver:x:\Windows\System32\pvpanic.inf
-if %gitbcommit%==win10 if exist x:\Windows\System32\qemupciserial.inf wget %giturl%/raw/%gitbcommit%/win10/drv/virtio-win-0.1.240/qemupciserial/%winversionmin%/%arch2%/qemupciserial.inf -O x:\Windows\System32\qemupciserial.inf
-if %gitbcommit%==win10 if exist x:\Windows\System32\qemupciserial.inf wget %giturl%/raw/%gitbcommit%/win10/drv/virtio-win-0.1.240/qemupciserial/%winversionmin%/%arch2%/qemupciserial.cat -O x:\Windows\System32\qemupciserial.cat
-if %gitbcommit%==win10 if exist x:\Windows\System32\qemupciserial.inf Dism /Image:W:\ /Add-Driver /Driver:x:\Windows\System32\qemupciserial.inf
-rem if %gitbcommit%==win7 if exist x:\Windows\System32\qxldod.inf wget %giturl%/raw/%gitbcommit%/win10/drv/virtio-win-0.1.240/qxl/%winversionmin%/%arch2%/qxl.cat -O x:\Windows\System32\qxl.cat
-rem if %gitbcommit%==win7 if exist x:\Windows\System32\qxldod.inf wget %giturl%/raw/%gitbcommit%/win10/drv/virtio-win-0.1.240/qxl/%winversionmin%/%arch2%/qxl.inf -O x:\Windows\System32\qxl.inf
-rem if %gitbcommit%==win7 if exist x:\Windows\System32\qxldod.inf wget %giturl%/raw/%gitbcommit%/win10/drv/virtio-win-0.1.240/qxl/%winversionmin%/%arch2%/qemupciserial.cat -O x:\Windows\System32\qemupciserial.cat
-rem if %gitbcommit%==win7 if exist x:\Windows\System32\qxldod.inf Dism /Image:W:\ /Add-Driver /Driver:x:\Windows\System32\qxl.inf
-if %gitbcommit%==win10 if exist x:\Windows\System32\qxldod.inf Dism /Image:W:\ /Add-Driver /Driver:x:\Windows\System32\qxldod.inf
-if %gitbcommit%==win10 if exist x:\Windows\System32\vioprot.inf Dism /Image:W:\ /Add-Driver /Driver:x:\Windows\System32\vioprot.inf
-if %gitbcommit%==win10 if exist x:\Windows\System32\viofs.inf Dism /Image:W:\ /Add-Driver /Driver:x:\Windows\System32\viofs.inf
-if %gitbcommit%==win10 if exist x:\Windows\System32\viogpudo.inf Dism /Image:W:\ /Add-Driver /Driver:x:\Windows\System32\viogpudo.inf
-if %gitbcommit%==win10 if exist x:\Windows\System32\vioinput.inf wget %giturl%/raw/%gitbcommit%/win10/drv/virtio-win-0.1.240/vioinput/%winversionmin%/%arch2%/vioinput.cat -O x:\Windows\System32\vioinput.cat
-if %gitbcommit%==win10 if exist x:\Windows\System32\vioinput.inf wget %giturl%/raw/%gitbcommit%/win10/drv/virtio-win-0.1.240/vioinput/%winversionmin%/%arch2%/vioinput.inf -O x:\Windows\System32\vioinput.inf
-if %gitbcommit%==win10 if exist x:\Windows\System32\vioinput.inf wget %giturl%/raw/%gitbcommit%/win10/drv/virtio-win-0.1.240/vioinput/%winversionmin%/%arch2%/vioinput.sys -O x:\Windows\System32\vioinput.sys
-if %gitbcommit%==win10 if exist x:\Windows\System32\vioinput.inf Dism /Image:W:\ /Add-Driver /Driver:x:\Windows\System32\vioinput.inf
-if %gitbcommit%==win10 if exist x:\Windows\System32\viorng.inf wget %giturl%/raw/%gitbcommit%/win10/drv/virtio-win-0.1.240/viorng/%winversionmin%/%arch2%/viorng.cat -O x:\Windows\System32\viorng.cat
-if %gitbcommit%==win10 if exist x:\Windows\System32\viorng.inf wget %giturl%/raw/%gitbcommit%/win10/drv/virtio-win-0.1.240/viorng/%winversionmin%/%arch2%/viorng.inf -O x:\Windows\System32\viorng.inf
-if %gitbcommit%==win10 if exist x:\Windows\System32\viorng.inf wget %giturl%/raw/%gitbcommit%/win10/drv/virtio-win-0.1.240/viorng/%winversionmin%/%arch2%/vioinput.sys -O x:\Windows\System32\viorng.sys
-if %gitbcommit%==win10 if exist x:\Windows\System32\viorng.inf Dism /Image:W:\ /Add-Driver /Driver:x:\Windows\System32\viorng.inf
-if %gitbcommit%==win10 if exist x:\Windows\System32\vioser.inf wget %giturl%/raw/%gitbcommit%/win10/drv/virtio-win-0.1.240/vioserial/%winversionmin%/%arch2%/vioser.cat -O x:\Windows\System32\vioser.cat
-if %gitbcommit%==win10 if exist x:\Windows\System32\vioser.inf wget %giturl%/raw/%gitbcommit%/win10/drv/virtio-win-0.1.240/vioserial/%winversionmin%/%arch2%/vioser.inf -O x:\Windows\System32\vioser.inf
-if %gitbcommit%==win10 if exist x:\Windows\System32\vioser.inf wget %giturl%/raw/%gitbcommit%/win10/drv/virtio-win-0.1.240/vioserial/%winversionmin%/%arch2%/vioser.sys -O x:\Windows\System32\vioser.sys
-if %gitbcommit%==win10 if exist x:\Windows\System32\vioser.inf Dism /Image:W:\ /Add-Driver /Driver:x:\Windows\System32\vioser.inf
+if exist x:\Windows\System32\fwcfg.inf Dism /Image:W:\ /Add-Driver /Driver:x:\Windows\System32\fwcfg.inf
+if exist x:\Windows\System32\pvpanic-pci.inf Dism /Image:W:\ /Add-Driver /Driver:x:\Windows\System32\pvpanic-pci.inf
+if exist x:\Windows\System32\pvpanic.inf Dism /Image:W:\ /Add-Driver /Driver:x:\Windows\System32\pvpanic.inf
+if exist x:\Windows\System32\qemupciserial.inf Dism /Image:W:\ /Add-Driver /Driver:x:\Windows\System32\qemupciserial.inf
+if exist x:\Windows\System32\qxldod.inf Dism /Image:W:\ /Add-Driver /Driver:x:\Windows\System32\qxldod.inf
+if exist x:\Windows\System32\vioprot.inf Dism /Image:W:\ /Add-Driver /Driver:x:\Windows\System32\vioprot.inf
+if exist x:\Windows\System32\viofs.inf Dism /Image:W:\ /Add-Driver /Driver:x:\Windows\System32\viofs.inf
+if exist x:\Windows\System32\viogpudo.inf Dism /Image:W:\ /Add-Driver /Driver:x:\Windows\System32\viogpudo.inf
+if exist x:\Windows\System32\vioinput.inf Dism /Image:W:\ /Add-Driver /Driver:x:\Windows\System32\vioinput.inf
+if exist x:\Windows\System32\viorng.inf Dism /Image:W:\ /Add-Driver /Driver:x:\Windows\System32\viorng.inf
+if exist x:\Windows\System32\vioser.inf Dism /Image:W:\ /Add-Driver /Driver:x:\Windows\System32\vioser.inf
 shutdown -r -t 1

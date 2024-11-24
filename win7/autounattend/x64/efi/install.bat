6c51d8f4dffc0b6d4f698a453e2f26f52024bd02
amidevous <amidevous@users.noreply.github.com>
2024-11-24 16:15:36 +0100

    Update install.bat

--- win7/autounattend/x64/efi/install.bat
+++ win7/autounattend/x64/efi/install.bat
@@ -51,6 +51,7 @@ for /f "delims=" %%a in ('ipconfig /all') do (
     )
 )
 rem pause
+setup-x86_64.exe --no-admin --root X:\Cygwin\ --no-shortcuts --no-startmenu --allow-unsupported-windows --arch x86_64 --force-current --no-desktop --no-replaceonreboot --no-verify --no-version-check --no-warn-deprecated-windows --no-write-registry --only-site --site %cygmirror% -l x:\Cygwin\var\cache\apt\packages --packages dos2unix,wget,ca-certificates
 x:\Cygwin\bin\wget.exe https://archive.mozilla.org/pub/firefox/releases/91.9.1esr/win64/fr/Firefox%20Setup%2091.9.1esr.exe -O "x:\Windows\System32\Firefox-Setup-91.9.1esr.exe"
 "x:\Windows\System32\Firefox-Setup-91.9.1esr.exe" /S
 "C:\Program Files\Mozilla Firefox\firefox.exe" https://www.driverscloud.com/fr/mes-pilotes
